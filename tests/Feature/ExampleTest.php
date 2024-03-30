ea7b7bb1e6bd9589d1bf672d56eb910a525bc74a
Abdulelah Adam <abdulelah859@gmail.com>
2024-03-30 02:25:14 +0200

    update

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -16,18 +16,6 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('http://mygym.test/');
 
-        if ($response->getStatusCode() === 302) {
-            // If redirected, attempt login with admin credentials
-            $response = $this->post('http://mygym.test/login', [
-                'email' => 'boss@admin.com',
-                'password' => 123,
-            ]);
-
-            // Assert that login was successful (replace 200 with actual success status code)
-            $response->assertStatus(200);
-        } else {
-            // If not redirected, assert the status code is 200
-            $response->assertStatus(200);
-        }
+        $response->assertStatus(302);
     }
 }
